5722a91971bc83a1fdbf74816f1332ad1ca271d8
ishi720 <ishizuka720@gmail.com>
2024-10-29 23:58:59 +0900

    テンプレートをレンダリングするように修正

--- main.go
+++ main.go
@@ -2,17 +2,37 @@ package main
 
 import (
 	"net/http"
-
 	"github.com/labstack/echo/v4"
+	"html/template"
+	"io"
 )
+
+// テンプレートの構造体
+type Template struct {
+	templates *template.Template
+}
+
+// テンプレートをレンダリング
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
 
 func main() {
 	// Echoインスタンスを作成
 	e := echo.New()
 
+	// Template構造体初期化し、HTMLテンプレートをロード
+	t := &Template{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
+	e.Renderer = t
+
 	// ルートハンドラを設定
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, Echo!")
+		data := map[string]interface{}{
+			"Message": "Hello, Echo!",
+		}
+		return c.Render(http.StatusOK, "index.html", data)
 	})
 
 	// サーバーをポート8080で起動
