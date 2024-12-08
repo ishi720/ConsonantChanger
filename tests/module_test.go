9b31abef0f5de278b4356ea3839dd94201b5618d
ishi720 <ishizuka720@gmail.com>
2024-12-08 09:49:35 +0900

    カタカナのテストケース追加

--- tests/module_test.go
+++ tests/module_test.go
@@ -26,15 +26,25 @@ func TestConsonantLockLanguage(t *testing.T) {
 		t.Errorf("文字列が一致しません。Result=%s, Expected=%s", result3, expected3)
 	}
 
+	// 記号のテスト
 	result4 := module.ConsonantLockLanguage("わたしは、おにぎりがたべたいです。", "ma")
 	expected4 := "ままみま、もみみみままめまみめむ。"
 	if result4 != expected4 {
 		t.Errorf("文字列が一致しません。Result=%s, Expected=%s", result4, expected4)
 	}
 
+	// 「っ」のテスト
 	result5 := module.ConsonantLockLanguage("こっぺぱん", "ya")
 	expected5 := "よっえやん"
 	if result5 != expected5 {
 		t.Errorf("文字列が一致しません。Result=%s, Expected=%s", result5, expected5)
 	}
+
+	// カタカナありのテスト
+	result6 := module.ConsonantLockLanguage("ドラえもん", "sa")
+	expected6 := "そさせそん"
+	if result6 != expected6 {
+		t.Errorf("文字列が一致しません。Result=%s, Expected=%s", result6, expected6)
+	}
+
 }
