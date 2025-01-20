6b7b84da73f78d0284020520318f720d8a390225
ishi720 <ishizuka720@gmail.com>
2025-01-20 22:35:48 +0900

    js部分を別ファイルに移動

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,35 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems);
+
+    // 入力時に変換ボタンの有効化
+    var inputString = document.getElementById("input_string");
+    inputString.addEventListener("input", function() {
+        document.getElementById("convert_btn").classList.toggle("disabled", inputString.value.length === 0);
+    });
+
+    // 変換ボタンのイベント
+    var convert_btn = document.getElementById("convert_btn");
+    var outputString = document.getElementById("result");
+    convert_btn.addEventListener("click", function() {
+        setTimeout(function() {
+            document.getElementById('copyButton').removeAttribute('hidden');
+        }, 500);
+    });
+
+    // コピーボタンのイベント
+    document.getElementById('copyButton').addEventListener('click', () => {
+        let textContent = document.getElementById("result").value;
+        navigator.clipboard.writeText(textContent)
+            .then(() => {
+                console.log('クリップボードにコピーしました:', textContent);
+            })
+            .catch(err => {
+                console.error('コピーに失敗しました:', err);
+            });
+    });
+});
+
 async function convertButton() {
     const inputString = document.getElementById("input_string").value;
     const lineType = document.getElementById("line_type").value;
